ce07deb792c70317efce59e40bc47bfec662bdd1
mrtbgau <mrt.bgau@gmail.com>
2024-10-17 20:43:36 +0200

    Add Role model

---
+++ Models/Role.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class Role
+    {
+        [Key]
+        public int RoleId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public virtual ICollection<RolePermission> RolePermissions { get; set; }
+        [Required]
+        public virtual ICollection<UserRole> UserRoles { get; set; }
+    }
+}
