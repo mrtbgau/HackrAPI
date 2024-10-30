3457dcba6ceb8b4bae5ecb67f150e2ce14a2923a
mrtbgau <mrt.bgau@gmail.com>
2024-10-30 23:22:54 +0100

    Interface LogService

---
+++ Services/Logs/ILogService.cs
@@ -0,0 +1,11 @@
+﻿using API.Models;
+
+namespace API.Services.Logs
+{
+    public interface ILogService
+    {
+        Task LogActionAsync(int userId, string action, string details);
+        Task<IEnumerable<Log>> GetRecentLogsAsync(int count);
+        Task<IEnumerable<Log>> GetUserLogsAsync(int userId, int count);
+    }
+}
