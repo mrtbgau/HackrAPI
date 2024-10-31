d6c57017a21e0698b3b5db40cb0c429aaf4abcd2
mrtbgau <mrt.bgau@gmail.com>
2024-10-31 10:28:39 +0100

    Add log controller

---
+++ Controllers/LogController.cs
@@ -0,0 +1,29 @@
+﻿using API.Models;
+using API.Services.JWT;
+using API.Services.Logs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogController(ILogService logService) : Controller
+    {
+        private readonly ILogService _logService = logService;
+
+        [Route("logs")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Log>>> GetRecentLogs([FromQuery] int count)
+        {
+            return Ok(await _logService.GetRecentLogs(count));
+        }
+
+        [Route("users/{id}/logs")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Log>>> GetUserLogs(int id, [FromQuery] int count)
+        {
+            return Ok(await _logService.GetUserLogs(id, count));
+        }
+    }
+}
