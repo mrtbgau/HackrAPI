6e00ab6a05207ccbbc8977f8bb1b348cb9dbe62c
mrtbgau <mrt.bgau@gmail.com>
2024-11-12 01:28:24 +0100

    Update bdd

--- Migrations/20241112000243_UpdateUserRoleRelationship.Designer.cs
+++ Migrations/20241112000243_UpdateUserRoleRelationship.Designer.cs
@@ -4,6 +4,7 @@ using API.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Migrations
 {
     [DbContext(typeof(DbAPIContext))]
-    partial class DbAPIContextModelSnapshot : ModelSnapshot
+    [Migration("20241112000243_UpdateUserRoleRelationship")]
+    partial class UpdateUserRoleRelationship
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20241112000243_UpdateUserRoleRelationship.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserRoleRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_logs_Users_UserId",
+                table: "logs");
+
+            migrationBuilder.DropTable(
+                name: "UserRoles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_logs",
+                table: "logs");
+
+            migrationBuilder.RenameTable(
+                name: "logs",
+                newName: "Logs");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_logs_UserId",
+                table: "Logs",
+                newName: "IX_Logs_UserId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoleId",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Logs",
+                table: "Logs",
+                column: "LogID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RoleId",
+                table: "Users",
+                column: "RoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Logs_Users_UserId",
+                table: "Logs",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Role_RoleId",
+                table: "Users",
+                column: "RoleId",
+                principalTable: "Role",
+                principalColumn: "RoleId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Logs_Users_UserId",
+                table: "Logs");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Role_RoleId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_RoleId",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Logs",
+                table: "Logs");
+
+            migrationBuilder.DropColumn(
+                name: "RoleId",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Logs",
+                newName: "logs");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Logs_UserId",
+                table: "logs",
+                newName: "IX_logs_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_logs",
+                table: "logs",
+                column: "LogID");
+
+            migrationBuilder.CreateTable(
+                name: "UserRoles",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    RoleId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRoles", x => new { x.UserId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_UserRoles_Role_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Role",
+                        principalColumn: "RoleId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserRoles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRoles_RoleId",
+                table: "UserRoles",
+                column: "RoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_logs_Users_UserId",
+                table: "logs",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
