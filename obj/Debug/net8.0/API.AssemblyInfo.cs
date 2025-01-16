3c102689443cf3a41f6cf90c52427169c6b7d965
mrtbgau <mrt.bgau@gmail.com>
2025-01-17 00:37:22 +0100

    update

--- obj/Debug/net8.0/API.AssemblyInfo.cs
+++ obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74c718662348b00d29d57725243a1678529e390b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c31cec34d06dc998b0166fa5da77d10896ba3534")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
